49743fe3330656e023760ee07f5aec4ca458045a
reoger <2454018075@qq.com>
2018-08-04 14:56:25 +0800

    first commit

---
+++ app/src/main/java/com/example/cm/circleview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cm.circleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cm.circleview.view.CircleImageView1;
+import com.example.cm.circleview.view.CircleImageView2;
+import com.example.cm.circleview.view.CircleImageView3;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String URL = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533911636&di=04ec95ec44a1623d52b20ff08727ac78&imgtype=jpg&er=1&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fitem%2F201312%2F05%2F20131205172342_P3nwv.jpeg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CircleImageView1 circleImageView1 = findViewById(R.id.image_id1);
+        circleImageView1.build(URL);
+
+        CircleImageView2 circleImageView2 = findViewById(R.id.image_id2);
+        circleImageView2.build(URL);
+
+        CircleImageView3 circleImageView3 =findViewById(R.id.image_id3);
+        circleImageView3.build(URL);
+
+    }
+}
---
+++ app/src/main/java/com/example/cm/circleview/view/CircleImageView1.java
@@ -0,0 +1,97 @@
+package com.example.cm.circleview.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.cm.circleview.R;
+
+import java.util.Arrays;
+
+/**
+ * Created by luojie on 2018/8/2.
+ * way 1. to use PorterDuffxfermode to show the photo
+ */
+
+public class CircleImageView1 extends ImageView {
+
+    private Paint mPaint;
+    private Shape mShape;
+    private float mRadius;
+
+    private float[] outerRadii = new float[8];;
+
+    public CircleImageView1(Context context) {
+        this(context,null);
+    }
+
+    public CircleImageView1(Context context, @Nullable AttributeSet attrs) {
+        this(context,attrs,0);
+    }
+
+    public CircleImageView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(attrs);
+    }
+
+    private void initView(AttributeSet attrs) {
+        if (attrs != null){
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CircleImageView1);
+            // 这里的代码
+            a.recycle();
+        }
+        setLayerType(LAYER_TYPE_HARDWARE,null);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        int min = Math.min(width,height);
+        mRadius = min/2;
+        setMeasuredDimension(min,min);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (changed){
+            if (mShape == null){
+                Arrays.fill(outerRadii,mRadius);
+                mShape = new RoundRectShape(outerRadii,null,null);
+            }
+            mShape.resize(getWidth(),getHeight());
+        }
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int saveCount = canvas.getSaveCount();
+        canvas.save();
+        super.onDraw(canvas);
+        if (mShape != null){
+            mShape.draw(canvas,mPaint);
+        }
+        canvas.restoreToCount(saveCount);
+    }
+
+    public void build(String url){
+        Glide.with(getContext()).load(url).into(this);
+    }
+}
